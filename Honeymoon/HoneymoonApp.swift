b938851996acdaa43bb38f6c0a32cc8bb02ad0d0
Jasmine <jasminehweying@gmail.com>
2023-06-14 18:00:18 +0800

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Jasmine Lai Hweeying on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
